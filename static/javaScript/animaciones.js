5d473eea681b4dd3612227fe4604d4b8ae6a3ad3
Jhonny Díaz Centeno <167951776+JhonnDiaz@users.noreply.github.com>
2024-04-27 21:46:46 -0500

    v.3

---
+++ static/javaScript/animaciones.js
@@ -0,0 +1,19 @@
+
+  function copyEmail( email ) {
+   
+    navigator.clipboard.writeText(email).then(function() {
+        alert('¡El correo electrónico ha sido copiado!');
+    }, function() {
+        alert('Lo siento, no se pudo copiar el correo electrónico. Puedes copiarlo manualmente.');
+    });
+}
+function downloadCV() {
+    var cvUrl = "../CV_JhonnyDiaz/Curriculum Jhonny.pdf";
+    var link = document.createElement("a");
+    link.href = cvUrl;
+    link.setAttribute("download", "");
+    link.style.display = "none";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
